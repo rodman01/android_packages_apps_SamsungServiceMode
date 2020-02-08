ee928716ac6a42be1d58f411aac0dd968e01f103
rodman01 <email4joern@arcor.de>
2020-02-08 23:04:07 +0100

    set: LOCAL_PRIVATE_PLATFORM_APIS := true for leos 16
    Change-Id: Ifdd3b052cd455753ae98b44c5fadd06ce63d8981

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SamsungServiceMode
 LOCAL_JAVA_LIBRARIES := telephony-common
